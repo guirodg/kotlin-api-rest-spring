18154d8afdabb206997e5a375066a52c844c5e89
guirodg <guilherme.rddev@gmail.com>
2021-06-01 17:11:15 -0300

    feat: GET - findByID

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
+class TopicoService(private var topicos: List<Topico>) {
+    init {
         val topico = Topico(
             id = 1,
             titulo = "Duvida Kotlin",
@@ -25,6 +25,47 @@ class TopicoService {
             )
         )
 
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin 2",
+            mensagem = "Variaveis no Kotlin 2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida Kotlin 3",
+            mensagem = "Variaveis no Kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da silva",
+                email = "ana@email.com"
+            )
+        )
+
+        topicos = Arrays.asList(topico, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter { t -> t.id == id
+        }.findFirst().get()
     }
 }
