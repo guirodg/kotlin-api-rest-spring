7d31adf3522067aa072e9ba30609bf62ea1171e5
guirodg <guilherme.rddev@gmail.com>
2021-06-02 14:04:18 -0300

    feat: tratamento de erro do bean validation

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,8 +5,12 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm(
-    @field:NotEmpty @field:Size(min = 5, max = 50) val titulo: String,
-    @field:NotEmpty val mensagem: String,
+    @field:NotEmpty(message = "Mensagem não pode ser em branco") @field:Size(
+        min = 5,
+        max = 50,
+        message = "Titulo deve ter entre 5 e 50 caracteres"
+    ) val titulo: String,
+    @field:NotEmpty(message = "Mensagem não pode ser em branco") val mensagem: String,
     @field:NotNull val idCurso: Long,
     @field:NotNull val idAutor: Long
 )
