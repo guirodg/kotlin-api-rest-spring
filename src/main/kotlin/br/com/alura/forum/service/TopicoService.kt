35bb0a04e46b4a83faf6bd6c3774251459673702
guirodg <guilherme.rddev@gmail.com>
2021-06-02 09:10:42 -0300

    feat: adiciona dto view

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,8 +1,10 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 import kotlin.collections.ArrayList
 
 @Service
@@ -10,25 +12,42 @@ class TopicoService(
     private var topicos: List<Topico> = ArrayList(),
     private val cursoService: CursoService,
     private val usuarioService: UsuarioService,
-    ) {
+) {
 
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map { t ->
+            TopicoView(
+                id = t.id,
+                titulo = t.titulo,
+                mensagem = t.mensagem,
+                dataCriacao = t.dataCriacao,
+                status = t.status
+            )
+        }.collect(Collectors.toList())
     }
 
-    fun buscarPorId(id: Long): Topico {
-        return topicos.stream().filter { t -> t.id == id
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
         }.findFirst().get()
+        return TopicoView(
+            id = topico.id,
+            titulo = topico.titulo,
+            mensagem = topico.mensagem,
+            dataCriacao = topico.dataCriacao,
+            status = topico.status
+        )
     }
 
-    fun cadastrar(dto: NovoTopicoDto) {
+    fun cadastrar(form: NovoTopicoForm) {
         topicos = topicos.plus(
             Topico(
                 id = topicos.size.toLong() + 1,
-                titulo = dto.titulo,
-                mensagem = dto.mensagem,
-                curso = cursoService.burcarPorId(dto.idCurso),
-                autor = usuarioService.burcarPorId(dto.idAutor),
-        ))
+                titulo = form.titulo,
+                mensagem = form.mensagem,
+                curso = cursoService.burcarPorId(form.idCurso),
+                autor = usuarioService.burcarPorId(form.idAutor),
+            )
+        )
     }
 }
