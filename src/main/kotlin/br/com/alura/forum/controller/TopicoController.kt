6d25c967543d7d727facdebba2b228c1601dba69
guirodg <guilherme.rddev@gmail.com>
2021-06-02 15:41:49 -0300

    feat: ajusta codigo service para acessar ao db

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriBuilder
 import org.springframework.web.util.UriComponentsBuilder
+import javax.transaction.Transactional
 import javax.validation.Valid
 
 @RestController
@@ -26,6 +27,7 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
+    @Transactional
     fun cadastrar(
         @RequestBody @Valid form: NovoTopicoForm,
         uriBuilder: UriComponentsBuilder
@@ -36,6 +38,7 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PutMapping
+    @Transactional
     fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView> {
         val topicoView = service.atualizar(form)
         return ResponseEntity.ok(topicoView)
@@ -43,6 +46,7 @@ class TopicoController(private val service: TopicoService) {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
     fun deletar(@PathVariable id: Long) {
         service.deletar(id)
     }
--- src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -1,24 +1,13 @@
 package br.com.alura.forum.service
 
 import br.com.alura.forum.model.Usuario
+import br.com.alura.forum.repository.UsuarioRepository
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
-class UsuarioService(var usuarios: List<Usuario>) {
-    init {
-        val usuario = Usuario(
-            id = 1,
-            nome = "Guilherme da silva",
-            email = "guilher@email.com.br"
-        )
-        usuarios = Arrays.asList(usuario)
-    }
+class UsuarioService(private val repository: UsuarioRepository) {
 
     fun burcarPorId(id: Long): Usuario {
-        return usuarios.stream().filter { c ->
-            c.id == id
-        }.findFirst().get()
+        return repository.getById(id)
     }
-
 }
