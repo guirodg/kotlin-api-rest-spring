10931d25da37b92ec937abb172d3392be8ce95a1
guirodg <guilherme.rddev@gmail.com>
2021-06-02 11:37:49 -0300

    feat: metodo atualizar PUT

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
@@ -23,5 +24,11 @@ class TopicoController(private val service: TopicoService) {
     @PostMapping
     fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
         service.cadastrar(form)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+        service.atualizar(form)
+
     }
 }
